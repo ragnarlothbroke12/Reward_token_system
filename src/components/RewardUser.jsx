2646869fd3ebc10c88e997f44176f791b88219d9
ragnarlothbroke12 <siyaram490don@gmail.com>
2025-11-10 12:41:42 +0530

    rewardUSer

--- src/components/RewardUser.jsx
+++ src/components/RewardUser.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function RewardUser() {
+const RewardUser = ({ contract, refreshBalance }) => {
+  const [userAddress, setUserAddress] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const rewardUser = async () => {
+    try {
+      const tx = await contract.rewardUser(userAddress, amount);
+      await tx.wait();
+      alert(` Rewarded ${userAddress} with ${amount} PRT successfully!`);
+      refreshBalance();
+      setUserAddress("");
+      setAmount("");
+    } catch (err) {
+      alert(" Only owner can reward users");
+      console.error(err);
+    }
+  };
+
   return (
-    <div>RewardUser</div>
-  )
-}
+    <div className="card">
+      <h3>Reward User </h3>
+      <input
+        type="text"
+        placeholder="User Wallet Address"
+        value={userAddress}
+        onChange={(e) => setUserAddress(e.target.value)}
+        className="input"
+      />
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        className="input"
+      />
+      <button onClick={rewardUser}>Reward</button>
+    </div>
+  );
+};
+
+export default RewardUser;
